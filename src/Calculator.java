0754d88d50c69d855a2d359fbdc97a23eff0cb50
Mohsin Alam <mohsin.alam@mobiledatatech.com>
2023-08-04 18:01:05 -0600

    Mohsins commit 2 on day 2

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//TESTING
+//TESTING1
 
 public class Calculator implements ActionListener {
 
